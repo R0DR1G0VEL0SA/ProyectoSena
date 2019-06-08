3d5102492b8b61a5226ab19ae137fbc1bfc525c8
jvelosa <jrvelosa@misena.edu.co>
2019-06-08 00:49:32 -0500

    Ajuste pantalla Home
    en proceso

--- Fuentes/Drive/src/app/tab2/tab2.module.ts
+++ Fuentes/Drive/src/app/tab2/tab2.module.ts
@@ -1,25 +1,24 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { Tab2PageRoutingModule } from './tab2.routing.module';
+
 import { Tab2Page } from './tab2.page';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: Tab2Page
-  }
-];
-
-
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
-  ],
-  declarations: [Tab2Page]
+  imports:
+    [
+      IonicModule,
+      CommonModule,
+      FormsModule,
+      Tab2PageRoutingModule
+    ],
+  declarations:
+    [
+      Tab2Page
+    ]
 })
-export class Tab2PageModule {}
+export class TabsPageModule {}
+
--- Fuentes/Drive/src/app/tab6/tab6.page.spec.ts
+++ Fuentes/Drive/src/app/tab6/tab6.page.spec.ts
@@ -1,21 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Tab2Page } from './tab2.page';
+import { Tab6Page } from './tab6.page';
 
-describe('Tab2Page', () => {
-  let component: Tab2Page;
-  let fixture: ComponentFixture<Tab2Page>;
+describe('Tab6Page', () => {
+  let component: Tab6Page;
+  let fixture: ComponentFixture<Tab6Page>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab2Page],
+      declarations: [ Tab6Page ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
+    })
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(Tab2Page);
+    fixture = TestBed.createComponent(Tab6Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
