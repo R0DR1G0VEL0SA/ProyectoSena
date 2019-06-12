01888a128d6e24efe9bd7828c25c4cd41d8f7eb9
henry <henry.gonzalez@tpcenlinea.com>
2019-06-11 20:57:00 -0500

    se coloca mapa en el rol empresa
    vista de mapa
    se installan dependencias:
    1-ionic cordova plugin add cordova-plugin-geolocation
    npm install --save @ionic-native/geolocation
    
    2-ionic cordova platform add android
    3-npm install @ionic-native/google-maps --save

--- Fuentes/Company/src/app/componentes/mapa/mapa.page.ts
+++ Fuentes/Company/src/app/componentes/mapa/mapa.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Geolocation } from '@ionic-native/geolocation/ngx';
+
+import {  LoadingController } from '@ionic/angular';
+
+
+declare var google;
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
@@ -7,9 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  mapRef = null;
+
+  constructor(
+    private geolocation: Geolocation,
+    private loadCtrl: LoadingController
+  ) { 
+    
+  }//se crea un metodo encargado de cargar el mapa
+    ngOnInit(){
+       this.loadMap();
+    }
+    async loadMap(){
+      //muestra el loading
+      const loading = await this.loadCtrl.create();
+      loading.present();
+   //  fin muestra el loading
+      const myLatLng= await this.getLocation();
+      const mapEle: HTMLElement = document.getElementById('map');
+      this.mapRef = new google.maps.Map(mapEle, {
+        center: myLatLng,
+        zoom:12
+      }); 
+        //muestra que se carga el mapa load
+      google.maps.event
+      .addListenerOnce(this.mapRef, 'idle', () =>{
+        loading.dismiss(); //se apaga el load
+        this.addMarker(myLatLng.lat, myLatLng.lng);
+      });
+  
+    }
+      //crear el marker con metodos
+      private addMarker(lat: number, lng: number){
+       const marker = new google.maps.Marker({
+         position:{ lat, lng },
+         map: this.mapRef,
+  
+       });
+     }
+    //metodo
+     private async getLocation(){
+       const rta = await this.geolocation.getCurrentPosition();
+       return {
+         lat: rta.coords.latitude,
+         lng: rta.coords.longitude
+       };
+  
+       
+     }
+    
+  
+     
+  
   }
-
-}
